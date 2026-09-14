534489b985ea891f8db1b664fc7f369d75500b15
Daniela Castaño Moreno <1113979182@u.icesi.edu.co>
2026-09-14 03:59:20 -0500

    fix: port mapping changed

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import type { Product } from '../types/product.ts';
 
-const IP_INSTANCE = '35.175.171.226'
+//const IP_INSTANCE = '35.175.171.226'
 const API_BASE_URL = (import.meta as any).env.VITE_API_URL || 'http://localhost:8080/api';
 
 export const ProductService = {
