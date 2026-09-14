e17210e4ab77db9986273442a91e526caae65002
Daniela Castaño Moreno <1113979182@u.icesi.edu.co>
2026-09-13 20:42:38 -0500

    fix: add global.d.ts file to specify what is the global .css file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App.js';
-//import './index.css';  Asi no se importa en .tsx
+import './index.css';
 // tsc: typescript compiler
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
